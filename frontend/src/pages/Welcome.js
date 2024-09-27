6d27b691d51ac4f4e80f78f9041104c6534c6cc3
ZMHossain <zihadmohammad1998@gmail.com>
2024-09-27 12:58:55 +0600

    Styling Welcome component

--- frontend/src/pages/Welcome.js
+++ frontend/src/pages/Welcome.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
-import { handleError } from "../util";
+import { handleError, handleSuccess } from "../util";
+import { ToastContainer } from "react-toastify";
 
 const Welcome = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
   const fetchProducts = async () => {
     try {
       const url = "http://localhost:8080/products/products";
@@ -27,27 +30,64 @@ const Welcome = () => {
   useEffect(() => {
     fetchProducts();
   }, []);
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+    handleSuccess("Product added to cart!");
+  };
+
   return (
     <>
-      <div className="navbar">
-        <Navbar />
+      <div className="navbar"></div>
+      <Navbar />
+      <div style={{ marginTop: "64px", padding: "20px" }}>
+        <h1>Welcome to NexCart</h1>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-around",
+          }}
+        >
+          {products &&
+            products?.map((product) => (
+              <div
+                key={product._id}
+                style={{
+                  width: "250px",
+                  margin: "10px",
+                  padding: "0px",
+                  border: "1px solid #ddd",
+                  borderRadius: "8px",
+                }}
+              >
+                <img
+                  src={product.imageUrl}
+                  alt={product.name}
+                  style={{ width: "100%", height: "250px", objectFit: "cover" }}
+                />
+                <p style={{ paddingLeft: "8px" }}>{product.name}</p>
+                <p style={{ paddingLeft: "8px" }}>{product.description}</p>
+                <p style={{ fontSize: "25px", padding: "5px" }}>
+                  <strong>${product.price}</strong>
+                </p>
+                {/* <button
+                  onClick={() => addToCart(product)}
+                  style={{
+                    backgroundColor: "#4CAF50",
+                    color: "white",
+                    padding: "10px",
+                    border: "none",
+                    borderRadius: "4px",
+                    cursor: "pointer",
+                  }}
+                >
+                  Add to Cart
+                </button> */}
+              </div>
+            ))}
+        </div>
       </div>
-      <h1>Welcome</h1>
-      <div>
-        {products &&
-          products?.map((product) => (
-            <div key={product._id}>
-              <h3>{product.name}</h3>
-              <p>{product.description}</p>
-              <p>${product.price}</p>
-              <img
-                src={product.imageUrl}
-                alt={product.name}
-                style={{ width: "200px" }}
-              />
-            </div>
-          ))}
-      </div>
+      <ToastContainer />
     </>
   );
 };
