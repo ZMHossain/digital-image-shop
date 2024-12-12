25f2c738ff9f361256a1db67869a81a1e400460d
ZMHossain <zihadmohammad1998@gmail.com>
2024-12-12 23:13:09 +0600

    updated userInfo

--- frontend/src/components/UserInfo.js
+++ frontend/src/components/UserInfo.js
@@ -1,15 +1,38 @@
 import React from "react";
 
 const UserInfo = ({ user }) => {
+  const sections = [
+    {
+      title: "Full Name",
+      content: user,
+      subtitle: "Personal Profile",
+      contents: "Birthday",
+    },
+    {
+      title: "Email Address",
+      content: user,
+      subtitle: "Receive marketing emails",
+      contents: "Gender",
+    },
+    {
+      title: "Mobile",
+      content: user,
+      subtitle: "Personal Profile",
+      contents: "Birthday",
+    },
+  ];
+  console.log(sections);
+
   return (
     <div className="flex flex-row">
-      {Array.from({ length: 3 }).map((_, index) => (
+      {sections.map((section, index) => (
         <div className="pr-16" key={index}>
-          <h1>Full name</h1>
-          <p>{user}</p>
-          <p>Personal Profile</p>
-          <button>Edit</button>
-          <p>Recieve Marketing Emails</p>
+          <h1>{section.title}</h1>
+          <p>{section.content}</p>
+          <div className="flex flex-row">
+            <p>{section.subtitle}</p>
+            <button className="flex pl-2 text-sky-900">Edit</button>
+          </div>
         </div>
       ))}
     </div>
